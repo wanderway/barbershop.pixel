2eb8886970e1a656737ea6b9dc7fb81825ca1a36
Yauheni Humanau <idracat@gmail.com>
2018-08-30 20:32:28 +0300

    adaptive features block
    with slider on mobile version

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+//= libs/jquery-3.3.1.min.js
+//= libs/slick.min.js
+//= partials/js.js
+
--- src/js/partials/js.js
+++ src/js/partials/js.js
@@ -1,3 +1,33 @@
 (function () {
 
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        $('.features__wrapper').slick({
+            settings: 'unslick',
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        dots: true,
+                        arrows: false,
+                        autoplay: true
+                    }
+                }
+            ]
+        });
+    }
 })();
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+    } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+    }
+});
